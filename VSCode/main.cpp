52345527d14a1f3cd5f12f86aa9fd82ea3cc419a
ads\taym <michael.tay@autodesk.com>
2017-03-12 20:59:04 +0800

    Sample VSCode project

---
+++ VSCode/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <limits>
+using namespace std;
+
+int main(void)
+{
+    cout << "Hi yo" << endl;
+    int a=5;
+    for(int i=a; i < 8; ++i)
+        cout << i << endl;
+
+    return 0;
+}
